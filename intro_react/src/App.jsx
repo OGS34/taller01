afc0d08254dfe6c9f9a5791d209b0c8bba1ce357
OGS34 <c27486@utp.edu.pe>
2024-05-19 22:14:55 -0500

    tres

---
+++ intro_react/src/App.jsx
@@ -0,0 +1,24 @@
+function Saludo (){
+  return
+    <h1>hola orlando oswaldo</h1>    
+}
+function Despedida (){
+  return
+    <h1>chao orlando oswaldo</h1>    
+}
+
+const App =()=>{
+  return(
+    <>
+    <h1>hola</h1>
+    <Saludo/>
+    <Despedida/>
+    </>
+  )
+}
+
+
+
+
+
+export default App
